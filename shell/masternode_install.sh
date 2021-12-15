163531283512a0bf27457925a9cc9af5b1d5f302
SparkbaseHub <44909308+SparkBaseHub@users.noreply.github.com>
2021-12-15 19:25:39 +0100

    Update masternode_install.sh

--- shell/masternode_install.sh
+++ shell/masternode_install.sh
@@ -243,8 +243,8 @@ fi
 
 
 function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+if [[ $(lsb_release -d) != *20.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 20.04. Installation is cancelled.${NC}"
   exit 1
 fi
 
