9740cb95333c03d8e9067373cf01dc5a7fc80bd2
SparkbaseHub <44909308+SparkBaseHub@users.noreply.github.com>
2021-12-15 19:43:08 +0100

    Update masternode_install.sh

--- shell/masternode_install.sh
+++ shell/masternode_install.sh
@@ -176,12 +176,12 @@ function create_key() {
    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
    exit 1
   fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
+  COINKEY=$($COIN_PATH$COIN_CLI createmasternodekey)
   if [ "$?" -gt "0" ];
     then
     echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
     sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
+    COINKEY=$($COIN_PATH$COIN_CLI createmasternodekey)
   fi
   $COIN_PATH$COIN_CLI stop
 fi
