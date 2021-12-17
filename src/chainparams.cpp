7349fe7c020e78ab76b15551a707b5c5bf6ca41b
SparkBaseHub <sparkbase08@gmail.com>
2021-12-17 15:14:28 +0100

    updated checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -70,8 +70,7 @@ static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits
  * @todo: change checkpoints
  */
  static Checkpoints::MapCheckpoints mapCheckpoints = {
-     //{ 202, uint256S("f89cae964f5586c65ae21bae0cce0984d2e115e633b886d40ca9771315b3d404")},
-     //{ 1000, uint256S("831779926bcb205e68e39751cd5d74a5460078fee3790f39dc860458bd6f7c50")},
+     { 210, uint256S("9884bdb1bfde5883248a10b6526a2c2aad839449d5e2aea6f40d3a969b448395")},
  };
 
 static const Checkpoints::CCheckpointData data = {
