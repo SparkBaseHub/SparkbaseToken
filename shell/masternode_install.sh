26a83ec49c1d1bd4706f2122d2cade1b6a6751df
SparkbaseHub <44909308+SparkBaseHub@users.noreply.github.com>
2021-12-19 21:33:49 +0100

    Update masternode_install.sh

--- shell/masternode_install.sh
+++ shell/masternode_install.sh
@@ -193,9 +193,9 @@ function update_config() {
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 logintimestamps=1
 maxconnections=256
-#bind=$NODEIP
 masternode=1
-externalip=$NODEIP:$COIN_PORT
+externalip=$NODEIP
+masternodeaddr=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
 EOF
 }
